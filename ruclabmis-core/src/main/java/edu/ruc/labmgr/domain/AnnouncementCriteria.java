8f8214e7aa89d7727a1f6dc40061aecb9dd724a9
sjzheng-arcie <sjzheng@arcie.org>
2013-10-16 16:05:27 +0800

    添加修改用户代码

---
+++ ruclabmis-core/src/main/java/edu/ruc/labmgr/domain/AnnouncementCriteria.java
@@ -0,0 +1,581 @@
+package edu.ruc.labmgr.domain;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class AnnouncementCriteria {
+    protected String orderByClause;
+
+    protected boolean distinct;
+
+    protected List<Criteria> oredCriteria;
+
+    public AnnouncementCriteria() {
+        oredCriteria = new ArrayList<Criteria>();
+    }
+
+    public void setOrderByClause(String orderByClause) {
+        this.orderByClause = orderByClause;
+    }
+
+    public String getOrderByClause() {
+        return orderByClause;
+    }
+
+    public void setDistinct(boolean distinct) {
+        this.distinct = distinct;
+    }
+
+    public boolean isDistinct() {
+        return distinct;
+    }
+
+    public List<Criteria> getOredCriteria() {
+        return oredCriteria;
+    }
+
+    public void or(Criteria criteria) {
+        oredCriteria.add(criteria);
+    }
+
+    public Criteria or() {
+        Criteria criteria = createCriteriaInternal();
+        oredCriteria.add(criteria);
+        return criteria;
+    }
+
+    public Criteria createCriteria() {
+        Criteria criteria = createCriteriaInternal();
+        if (oredCriteria.size() == 0) {
+            oredCriteria.add(criteria);
+        }
+        return criteria;
+    }
+
+    protected Criteria createCriteriaInternal() {
+        Criteria criteria = new Criteria();
+        return criteria;
+    }
+
+    public void clear() {
+        oredCriteria.clear();
+        orderByClause = null;
+        distinct = false;
+    }
+
+    protected abstract static class GeneratedCriteria {
+        protected List<Criterion> criteria;
+
+        protected GeneratedCriteria() {
+            super();
+            criteria = new ArrayList<Criterion>();
+        }
+
+        public boolean isValid() {
+            return criteria.size() > 0;
+        }
+
+        public List<Criterion> getAllCriteria() {
+            return criteria;
+        }
+
+        public List<Criterion> getCriteria() {
+            return criteria;
+        }
+
+        protected void addCriterion(String condition) {
+            if (condition == null) {
+                throw new RuntimeException("Value for condition cannot be null");
+            }
+            criteria.add(new Criterion(condition));
+        }
+
+        protected void addCriterion(String condition, Object value, String property) {
+            if (value == null) {
+                throw new RuntimeException("Value for " + property + " cannot be null");
+            }
+            criteria.add(new Criterion(condition, value));
+        }
+
+        protected void addCriterion(String condition, Object value1, Object value2, String property) {
+            if (value1 == null || value2 == null) {
+                throw new RuntimeException("Between values for " + property + " cannot be null");
+            }
+            criteria.add(new Criterion(condition, value1, value2));
+        }
+
+        public Criteria andIdIsNull() {
+            addCriterion("id is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdIsNotNull() {
+            addCriterion("id is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdEqualTo(Integer value) {
+            addCriterion("id =", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdNotEqualTo(Integer value) {
+            addCriterion("id <>", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdGreaterThan(Integer value) {
+            addCriterion("id >", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdGreaterThanOrEqualTo(Integer value) {
+            addCriterion("id >=", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdLessThan(Integer value) {
+            addCriterion("id <", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdLessThanOrEqualTo(Integer value) {
+            addCriterion("id <=", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdIn(List<Integer> values) {
+            addCriterion("id in", values, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdNotIn(List<Integer> values) {
+            addCriterion("id not in", values, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdBetween(Integer value1, Integer value2) {
+            addCriterion("id between", value1, value2, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdNotBetween(Integer value1, Integer value2) {
+            addCriterion("id not between", value1, value2, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleIsNull() {
+            addCriterion("title is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleIsNotNull() {
+            addCriterion("title is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleEqualTo(String value) {
+            addCriterion("title =", value, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleNotEqualTo(String value) {
+            addCriterion("title <>", value, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleGreaterThan(String value) {
+            addCriterion("title >", value, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleGreaterThanOrEqualTo(String value) {
+            addCriterion("title >=", value, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleLessThan(String value) {
+            addCriterion("title <", value, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleLessThanOrEqualTo(String value) {
+            addCriterion("title <=", value, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleLike(String value) {
+            addCriterion("title like", value, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleNotLike(String value) {
+            addCriterion("title not like", value, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleIn(List<String> values) {
+            addCriterion("title in", values, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleNotIn(List<String> values) {
+            addCriterion("title not in", values, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleBetween(String value1, String value2) {
+            addCriterion("title between", value1, value2, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andTitleNotBetween(String value1, String value2) {
+            addCriterion("title not between", value1, value2, "title");
+            return (Criteria) this;
+        }
+
+        public Criteria andContentIsNull() {
+            addCriterion("content is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andContentIsNotNull() {
+            addCriterion("content is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andContentEqualTo(String value) {
+            addCriterion("content =", value, "content");
+            return (Criteria) this;
+        }
+
+        public Criteria andContentNotEqualTo(String value) {
+            addCriterion("content <>", value, "content");
+            return (Criteria) this;
+        }
+
+        public Criteria andContentGreaterThan(String value) {
+            addCriterion("content >", value, "content");
+            return (Criteria) this;
+        }
+
+        public Criteria andContentGreaterThanOrEqualTo(String value) {
+            addCriterion("content >=", value, "content");
+            return (Criteria) this;
+        }
+
+        public Criteria andContentLessThan(String value) {
+            addCriterion("content <", value, "content");
+            return (Criteria) this;
+        }
+
+        public Criteria andContentLessThanOrEqualTo(String value) {
+            addCriterion("content <=", value, "content");
+            return (Criteria) this;
+        }
+
+        public Criteria andContentLike(String value) {
+            addCriterion("content like", value, "content");
+            return (Criteria) this;
+        }
+
+        public Criteria andContentNotLike(String value) {
+            addCriterion("content not like", value, "content");
+            return (Criteria) this;
+        }
+
+        public Criteria andContentIn(List<String> values) {
+            addCriterion("content in", values, "content");
+            return (Criteria) this;
+        }
+
+        public Criteria andContentNotIn(List<String> values) {
+            addCriterion("content not in", values, "content");
+            return (Criteria) this;
+        }
+
+        public Criteria andContentBetween(String value1, String value2) {
+            addCriterion("content between", value1, value2, "content");
+            return (Criteria) this;
+        }
+
+        public Criteria andContentNotBetween(String value1, String value2) {
+            addCriterion("content not between", value1, value2, "content");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublisherIdIsNull() {
+            addCriterion("publisher_id is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublisherIdIsNotNull() {
+            addCriterion("publisher_id is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublisherIdEqualTo(Integer value) {
+            addCriterion("publisher_id =", value, "publisherId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublisherIdNotEqualTo(Integer value) {
+            addCriterion("publisher_id <>", value, "publisherId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublisherIdGreaterThan(Integer value) {
+            addCriterion("publisher_id >", value, "publisherId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublisherIdGreaterThanOrEqualTo(Integer value) {
+            addCriterion("publisher_id >=", value, "publisherId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublisherIdLessThan(Integer value) {
+            addCriterion("publisher_id <", value, "publisherId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublisherIdLessThanOrEqualTo(Integer value) {
+            addCriterion("publisher_id <=", value, "publisherId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublisherIdIn(List<Integer> values) {
+            addCriterion("publisher_id in", values, "publisherId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublisherIdNotIn(List<Integer> values) {
+            addCriterion("publisher_id not in", values, "publisherId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublisherIdBetween(Integer value1, Integer value2) {
+            addCriterion("publisher_id between", value1, value2, "publisherId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublisherIdNotBetween(Integer value1, Integer value2) {
+            addCriterion("publisher_id not between", value1, value2, "publisherId");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishTimeIsNull() {
+            addCriterion("publish_time is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishTimeIsNotNull() {
+            addCriterion("publish_time is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishTimeEqualTo(Date value) {
+            addCriterion("publish_time =", value, "publishTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishTimeNotEqualTo(Date value) {
+            addCriterion("publish_time <>", value, "publishTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishTimeGreaterThan(Date value) {
+            addCriterion("publish_time >", value, "publishTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishTimeGreaterThanOrEqualTo(Date value) {
+            addCriterion("publish_time >=", value, "publishTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishTimeLessThan(Date value) {
+            addCriterion("publish_time <", value, "publishTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishTimeLessThanOrEqualTo(Date value) {
+            addCriterion("publish_time <=", value, "publishTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishTimeIn(List<Date> values) {
+            addCriterion("publish_time in", values, "publishTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishTimeNotIn(List<Date> values) {
+            addCriterion("publish_time not in", values, "publishTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishTimeBetween(Date value1, Date value2) {
+            addCriterion("publish_time between", value1, value2, "publishTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishTimeNotBetween(Date value1, Date value2) {
+            addCriterion("publish_time not between", value1, value2, "publishTime");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishLimitIsNull() {
+            addCriterion("publish_limit is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishLimitIsNotNull() {
+            addCriterion("publish_limit is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishLimitEqualTo(Integer value) {
+            addCriterion("publish_limit =", value, "publishLimit");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishLimitNotEqualTo(Integer value) {
+            addCriterion("publish_limit <>", value, "publishLimit");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishLimitGreaterThan(Integer value) {
+            addCriterion("publish_limit >", value, "publishLimit");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishLimitGreaterThanOrEqualTo(Integer value) {
+            addCriterion("publish_limit >=", value, "publishLimit");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishLimitLessThan(Integer value) {
+            addCriterion("publish_limit <", value, "publishLimit");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishLimitLessThanOrEqualTo(Integer value) {
+            addCriterion("publish_limit <=", value, "publishLimit");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishLimitIn(List<Integer> values) {
+            addCriterion("publish_limit in", values, "publishLimit");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishLimitNotIn(List<Integer> values) {
+            addCriterion("publish_limit not in", values, "publishLimit");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishLimitBetween(Integer value1, Integer value2) {
+            addCriterion("publish_limit between", value1, value2, "publishLimit");
+            return (Criteria) this;
+        }
+
+        public Criteria andPublishLimitNotBetween(Integer value1, Integer value2) {
+            addCriterion("publish_limit not between", value1, value2, "publishLimit");
+            return (Criteria) this;
+        }
+    }
+
+    public static class Criteria extends GeneratedCriteria {
+
+        protected Criteria() {
+            super();
+        }
+    }
+
+    public static class Criterion {
+        private String condition;
+
+        private Object value;
+
+        private Object secondValue;
+
+        private boolean noValue;
+
+        private boolean singleValue;
+
+        private boolean betweenValue;
+
+        private boolean listValue;
+
+        private String typeHandler;
+
+        public String getCondition() {
+            return condition;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        public Object getSecondValue() {
+            return secondValue;
+        }
+
+        public boolean isNoValue() {
+            return noValue;
+        }
+
+        public boolean isSingleValue() {
+            return singleValue;
+        }
+
+        public boolean isBetweenValue() {
+            return betweenValue;
+        }
+
+        public boolean isListValue() {
+            return listValue;
+        }
+
+        public String getTypeHandler() {
+            return typeHandler;
+        }
+
+        protected Criterion(String condition) {
+            super();
+            this.condition = condition;
+            this.typeHandler = null;
+            this.noValue = true;
+        }
+
+        protected Criterion(String condition, Object value, String typeHandler) {
+            super();
+            this.condition = condition;
+            this.value = value;
+            this.typeHandler = typeHandler;
+            if (value instanceof List<?>) {
+                this.listValue = true;
+            } else {
+                this.singleValue = true;
+            }
+        }
+
+        protected Criterion(String condition, Object value) {
+            this(condition, value, null);
+        }
+
+        protected Criterion(String condition, Object value, Object secondValue, String typeHandler) {
+            super();
+            this.condition = condition;
+            this.value = value;
+            this.secondValue = secondValue;
+            this.typeHandler = typeHandler;
+            this.betweenValue = true;
+        }
+
+        protected Criterion(String condition, Object value, Object secondValue) {
+            this(condition, value, secondValue, null);
+        }
+    }
+}
---
+++ ruclabmis-core/src/main/java/edu/ruc/labmgr/mapper/AnnouncementMapper.java
@@ -0,0 +1,33 @@
+package edu.ruc.labmgr.mapper;
+
+import edu.ruc.labmgr.domain.Announcement;
+import edu.ruc.labmgr.domain.AnnouncementCriteria;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
+
+public interface AnnouncementMapper {
+    int countByCriteria(AnnouncementCriteria Criteria);
+
+    int deleteByCriteria(AnnouncementCriteria Criteria);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Announcement record);
+
+    int insertSelective(Announcement record);
+
+    List<Announcement> selectByCriteriaWithRowbounds(AnnouncementCriteria Criteria, RowBounds rowBounds);
+
+    List<Announcement> selectByCriteria(AnnouncementCriteria Criteria);
+
+    Announcement selectByPrimaryKey(Integer id);
+
+    int updateByCriteriaSelective(@Param("record") Announcement record, @Param("Criteria") AnnouncementCriteria Criteria);
+
+    int updateByCriteria(@Param("record") Announcement record, @Param("Criteria") AnnouncementCriteria Criteria);
+
+    int updateByPrimaryKeySelective(Announcement record);
+
+    int updateByPrimaryKey(Announcement record);
+}
