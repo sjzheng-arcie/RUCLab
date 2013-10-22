584780962dd29dc3a2f87c4b54e29b05b80b552b
jinlytin <826174551@qq.com>
2013-10-22 14:42:10 +0800

    更新代码前提交代码

---
+++ ruclabmis-core/src/main/java/edu/ruc/labmgr/service/AnnouncementService.java
@@ -0,0 +1,65 @@
+package edu.ruc.labmgr.service;
+
+import edu.ruc.labmgr.domain.Announcement;
+import edu.ruc.labmgr.domain.AnnouncementCriteria;
+import edu.ruc.labmgr.mapper.AnnouncementMapper;
+import edu.ruc.labmgr.utils.page.ObjectListPage;
+import edu.ruc.labmgr.utils.SysUtil;
+import edu.ruc.labmgr.utils.page.PageInfo;
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jinlytin
+ * Date: 13-10-16
+ * Time: 下午4:08
+ * To change this template use File | Settings | File Templates.
+ */
+
+@SuppressWarnings("ALL")
+@Service
+public class AnnouncementService {
+	@Autowired
+	private AnnouncementMapper mapperAnnouncement;
+
+	public ObjectListPage<Announcement> selectListPage(int currentPage, AnnouncementCriteria criteria){
+
+
+		ObjectListPage<Announcement> retList = null;
+		try {
+			String count = SysUtil.getConfigValue("showCount", "10");
+
+			int limit = Integer.valueOf(count);
+			int currentResult = (currentPage - 1) * limit;
+			int totalCount = mapperAnnouncement.countByCriteria(criteria);
+			int pageCount = (totalCount % limit == 0) ? (totalCount / limit) : (1 + totalCount / limit);
+
+			PageInfo pageInfo = new PageInfo();
+			pageInfo.setTotalResult(totalCount);
+			pageInfo.setTotalPage(pageCount);
+			pageInfo.setCurrentPage(currentPage);
+
+			RowBounds bounds = new RowBounds(currentResult, limit);
+			List<Announcement> announcementList= mapperAnnouncement.selectByCriteriaWithRowbounds(criteria, bounds);
+
+			retList = new ObjectListPage(pageInfo, announcementList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return retList;
+	}
+	public int insert(Announcement announcement) {
+		int result = 0;
+		try {
+			result = mapperAnnouncement.insert(announcement);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+}
---
+++ ruclabmis-core/src/main/java/edu/ruc/labmgr/service/MessageService.java
@@ -0,0 +1,64 @@
+package edu.ruc.labmgr.service;
+
+import edu.ruc.labmgr.domain.Message;
+import edu.ruc.labmgr.domain.MessageCriteria;
+import edu.ruc.labmgr.mapper.MessageMapper;
+import edu.ruc.labmgr.utils.SysUtil;
+import edu.ruc.labmgr.utils.page.ObjectListPage;
+import edu.ruc.labmgr.utils.page.PageInfo;
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jinlytin
+ * Date: 13-10-18
+ * Time: 下午2:52
+ * To change this template use File | Settings | File Templates.
+ */
+@SuppressWarnings("ALL")
+@Service
+public class MessageService {
+	@Autowired
+	private MessageMapper messageMapper;
+	public ObjectListPage<Message> selectListPage(int currentPage, MessageCriteria criteria){
+
+
+		ObjectListPage<Message> retList = null;
+		try {
+			String count = SysUtil.getConfigValue("showCount", "10");
+
+			int limit = Integer.valueOf(count);
+			int currentResult = (currentPage - 1) * limit;
+			int totalCount = messageMapper.countByCriteria(criteria);
+			int pageCount = (totalCount % limit == 0) ? (totalCount / limit) : (1 + totalCount / limit);
+
+			PageInfo pageInfo = new PageInfo();
+			pageInfo.setTotalResult(totalCount);
+			pageInfo.setTotalPage(pageCount);
+			pageInfo.setCurrentPage(currentPage);
+
+			RowBounds bounds = new RowBounds(currentResult, limit);
+			List<Message> messageList= messageMapper.selectByCriteriaWithRowbounds(criteria, bounds);
+
+			retList = new ObjectListPage(pageInfo, messageList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return retList;
+	}
+
+	public int insert(Message message) {
+		int result = 0;
+		try {
+			result = messageMapper.insert(message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+}
